701b4827cc927851f9e83b1d2e755daba2cb0906
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-12-06 16:08:13 +0200

    task 1

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -1,8 +1,21 @@
-var teacher3 = {
-    firstName: "John",
-    fullTimeEmployee: false,
-    lastName: "Doe",
-    location: "London",
-    contract: false,
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.StudentClass = exports.printTeacher = void 0;
+var printTeacher = function (firstName, lastName) {
+    return "".concat(firstName[0], ". ").concat(lastName);
 };
-console.log(teacher3);
+exports.printTeacher = printTeacher;
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return "Currently working";
+    };
+    StudentClass.prototype.displayName = function () {
+        return this.firstName;
+    };
+    return StudentClass;
+}());
+exports.StudentClass = StudentClass;
