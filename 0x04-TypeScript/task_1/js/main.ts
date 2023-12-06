fb014c930b9a04b0fb43ee5b9f9ac9aa964175c6
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-12-06 16:05:29 +0200

    task 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,3 +11,11 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
